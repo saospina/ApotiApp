dc26c55ae99bac99166973396a1e8c6528fc535a
sergio-ospina-yuxi <sergio.ospina@yuxiglobal.com>
2018-11-07 14:27:20 -0500

    Introduccion, how to create a http request

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  paises: any[] = [];
+
+  constructor( private http: HttpClient) {
+    
+    console.log('Doing a http request');
+    this.http.get('https://restcountries.eu/rest/v2/lang/es').subscribe( (response: any) => {
+      this.paises = response;
+      console.log(response);
+      
+    });
+   }
 
   ngOnInit() {
   }
