7f09d72e857fe6cb405e0d9ae388a03206a35447
sergio-ospina-yuxi <sergio.ospina@yuxiglobal.com>
2018-11-07 11:36:51 -0500

    Creating routes and adding a navbar to route to the components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+import { ArtistaComponent } from './components/artista/artista.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { importType } from '@angular/compiler/src/output/output_ast';
+
+
+// Import routes
+import { ROUTES } from './app.routs';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    ArtistaComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot( ROUTES, { useHash: true} )
   ],
   providers: [],
   bootstrap: [AppComponent]
